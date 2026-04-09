de0882fe5c92734f06f383d1f88e8f5c7f7add1e
anagsoroiu <ana21censal@gmail.com>
2026-04-09 10:31:34 +0200

    Proyecto 1 Actualizado con Storage - Finalizado

---
+++ 20-PROYECTO-Carrito-LS/js/app.js
@@ -0,0 +1,129 @@
+// variables
+const carrito = document.querySelector('#carrito'),
+      listaCursos = document.querySelector('#lista-cursos'),
+      contenedorCarrito = document.querySelector('#lista-carrito tbody'),
+      vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+
+let articulosCarrito = [];
+
+cargarEventListeners();
+function cargarEventListeners() {
+    // Cuando agregas un curso presionando "Agregar al Carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+
+    // Elimina cursos del carrito
+    carrito.addEventListener('click', eliminarCurso);
+
+    // Muestra los cursos del local storage
+    document.addEventListener('DOMContentLoaded', ()=>{
+        articulosCarrito = JSON.parse(localStorage.getItem('carrito'))
+            || [];
+
+        carritoHTML();
+    });
+
+    // Vaciar el carrito
+    vaciarCarritoBtn.addEventListener('click', ()=>{
+        articulosCarrito = []; // reseteamos el array
+
+        limpiarHTML(); // eliminamos todo el html
+    });
+}
+
+//Funciones
+function agregarCurso(e){
+    e.preventDefault();
+
+    if(e.target.classList.contains('agregar-carrito')){
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+// Elimina un curso del carrito
+function eliminarCurso(e){
+    if(e.target.classList.contains('borrar-curso')){
+        const cursoId = e.target.getAttribute('data-id');
+
+        // Elimina del arreglo de articulosCarrito por el data-id
+        articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
+
+        carritoHTML(); // Iterar sobre el carrito y mostrar en el HTML
+    }
+}
+
+// Lee el contenido del HTML al que le dimos click y 
+// extrae la información del curso
+function leerDatosCurso(curso){
+    // Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1,
+    }
+
+    // Revisa si un elemento ya existe en el carrito
+    const existe = articulosCarrito.some(curso => curso.id === infoCurso.id);
+    if(existe){
+        //Actualizamos la cantidad
+        const cursos = articulosCarrito.map(curso => {
+            if(curso.id === infoCurso.id){
+                curso.cantidad++;
+                return curso; // retorna el obj actualizado
+            } else {
+                return curso; // returna los obj que no son los duplicados
+            }
+        });
+        articulosCarrito = [...cursos];
+    } else {
+        // Agrega elementos al array de carrito
+        articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+
+    carritoHTML();
+}
+
+// Muestra el carrito de compras en el HTML
+function carritoHTML(){
+    // Limpiar el HML
+    limpiarHTML();
+
+    // Recorre el carrito y genera el HTML
+    articulosCarrito.forEach(curso =>{
+        const {imagen, titulo, precio, cantidad, id} = curso;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src="${imagen}" width="100" >
+            </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}">X</a>
+            </td>
+        `;
+
+        // Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    });
+
+    // Agregar el carrito de compras al Storage
+    sincronizarStorage();
+}
+
+function sincronizarStorage(){
+    localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML(){
+    // Forma lenta 
+    // contenedorCarrito.innerHTML = '';
+
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
